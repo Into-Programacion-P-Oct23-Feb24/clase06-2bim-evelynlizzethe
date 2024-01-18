228f30d87909dc550cb151c0f29e342e6541606a
evelynlizzethe <elriofrio1@utpl.edu.ec>
2024-01-18 15:47:51 -0500

    clase6-2bim

--- EjemplosDos/src/paquete01/Principal.java
+++ EjemplosDos/src/paquete01/Principal.java
@@ -6,9 +6,11 @@
 package paquete01;
 
 import paquete02.DatoPersonal;
+import paquete02.DatoTrabajo;
 import paquete03.DatosUbicacion;
 import paquete04.DatoAcademico;
 import paquete05.DatoFinal;
+
 
 /**
  *
@@ -20,26 +22,41 @@ public class Principal {
         String nombre;
         String ciudad;
         String apellidoRetornado;
+        String empresa; 
+        String direccion; 
+        String acumulador = "";
         
-        double[] misNotas;
+        double[] misNotas; 
         double promedio;
         
         nombre = DatoPersonal.obtenerNombre();
         apellidoRetornado = DatoPersonal.obtenerApellido();
+        empresa = DatoTrabajo.obtenerEmpresa();
+        direccion = DatoTrabajo.obtenerDireccion();
         ciudad = DatosUbicacion.obtenerCiudad();
         misNotas = DatoAcademico.obtenerNotas(4);
         promedio = DatoFinal.obtenerPromedio(misNotas);
         
-        
+        for (int i = 0; i < misNotas.length; i++ ){
+            acumulador = String.format("%s\nNotas %s: %.2f,  ", 
+                    acumulador, i +1,  misNotas[i]);
+        }
         System.out.printf("Los datos ingresados son:\n"
                 + "Nombre: %s\n"
                 + "Apellido: %s\n"
                 + "Ciudad: %s\n"
-                + "Promedio: %.2f\n",
+                + "Notas: %s\n"
+                + "Promedio: %.2f\n"
+                + "Nombre de la empresa: %s\n"
+                + "Direccion: %s\n",
                 nombre,
                 apellidoRetornado,
                 ciudad,
+                misNotas,
+                empresa,
+                direccion,
                 promedio);
+        System.out.printf("%s\n", acumulador);
     }
     
 }
